1e03fdf7957ea4b1a8debd46bac33b101e86812d
Jordan Kline <jordan.kline@gmail.com>
2018-01-11 21:10:22 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -11,10 +11,6 @@ include Paramable
 
   @@artists = []
 
-  # def self.find_by_name(name)
-  #   @@artists.detect{|a| a.name == name}
-  # end
-
   def initialize
     @@artists << self
     @songs = []
@@ -24,14 +20,6 @@ include Paramable
     @@artists
   end
 
-  # def self.reset_all
-  #   self.all.clear
-  # end
-
-  # def self.count
-  #   @@artists.count
-  # end
-
   def add_song(song)
     @songs << song
     song.artist = self
@@ -40,9 +28,4 @@ include Paramable
   def add_songs(songs)
     songs.each { |song| add_song(song) }
   end
-
-  # def to_param
-  #   name.downcase.gsub(' ', '-')
-  # end
-
 end
